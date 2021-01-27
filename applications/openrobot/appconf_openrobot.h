e3987f6c03a2a946f7a30b167588ec0ee46edbd4
dongilc <dongilc@mju.ac.kr>
2021-01-27 09:50:59 +0900

    20210127

--- applications/openrobot/appconf_openrobot.h
+++ applications/openrobot/appconf_openrobot.h
@@ -1,14 +1,14 @@
 /*
-	Copyright 2016 Benjamin Vedder	benjamin@vedder.se
+	Copyright 2021 Dongil Choi	drclab2018@gmail.com
 
-	This file is part of the VESC firmware.
+	This file is part of the OpenRobot Custom App.
 
-	The VESC firmware is free software: you can redistribute it and/or modify
+	The OpenRobot Custom App is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    The VESC firmware is distributed in the hope that it will be useful,
+    The OpenRobot Custom App is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
